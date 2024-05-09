36beb6a9939e670319cdad73b96265a83de428cf
Mr. Shemlo <avri.d@sygnia.co>
2024-05-09 09:59:13 +0300

    - Refined inline comment - Extracted agent heartbeat interval to a constant

--- sygnia-agent/src/main.rs
+++ sygnia-agent/src/main.rs
@@ -4,29 +4,30 @@ use std::time::Duration;
 
 use serde_json::json;
 
-// In this exercise you will implement the business logic of a simple Rust agent.
-//
-// The agent's main task is to periodically collect as much data as it can from the host it's
-// running on and send it to the server as part of a `heartbeat` request (defined to be sent every
-// 15 minutes)
-//
-// Your task is to implement as many collectors as you can (in the given time). Ideas for
-// collectors:
-// * Disk usage statistics
-// * Memory usage statistics
-// * Network interfaces list
-// * Disk volumes list
-// (Or anything else you think that's relevant)
-//
-// Few tips before you go -
-// * The server is congifured to log the body of the requests it gets. Make sure it looks like
-// you'd expect.
-// * Feel free to install and import any external package that might help you.
-// * Try to implement as many as possible.
+/***********************************************************************************************************************
+In this exercise you will implement the business logic of a simple Rust agent.
 
+The agent's main task is to periodically collect as much data as it can from the host it's running on and send it to
+the server as part of a `heartbeat` request. This request should be sent every 15 minutes to ensure up-to-date
+information.
+
+Your task is to implement as many collectors as you can (in the given time). Ideas for collectors:
+* Disk usage statistics
+* Memory usage statistics
+* Network interfaces list
+* Disk volumes list
+(Or anything else you may come up with)
+
+A few tips before you go:
+1. The server is configured to log the body of the requests it receives. Make sure that the data sent by your agent
+appears as expected in these logs.
+2. Feel free to install and import any external packages that may assist you in implementing the collectors.
+3. Aim to implement as many collectors as possible within the given time constraints.
+***********************************************************************************************************************/
 
 // Specify the URL to which you want to send the heartbeat request
 const SERVER_URL: &str = "http://127.0.0.1:5000/heartbeat";
+const HEARTBEAT_INTERVAL: u32 = 15;
 
 fn main() {
     // Infinite loop
@@ -38,7 +39,7 @@ fn main() {
         let _ = send_heartbeat_request(SERVER_URL);
 
         // Wait for 15 minutes before sending the next heartbeat
-        std::thread::sleep(Duration::from_secs(15));
+        std::thread::sleep(Duration::from_secs(HEARTBEAT_INTERVAL));
     }
 }
 
